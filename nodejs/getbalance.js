4f2649fe8b7931e46bf6a5f2fe99d04dc4a670c8
James Brotchie <brotchie@gmail.com>
2014-01-30 12:01:00 +1000

    Added some comments.

--- nodejs/getbalance.js
+++ nodejs/getbalance.js
@@ -1,3 +1,13 @@
+/* A "Hello World" example of using node-dogecoin to access
+ * dogecoind via JSON-RPC. Retreives the current wallet balance.
+ *
+ * Either run dogecoind directly or run dogecoin-qt with the -server
+ * command line option. Make sure you have a ~/.dogecoin/dogecoin.conf
+ * with rpcuser and rpcpassword config values filled out. Note that
+ * newer versions of dogecoin (1.5 and above) don't allow identical
+ * your rpc username and password to be identical.
+ *
+ */
 
 /* Copy config.json.template to config.json and fill in your
  * rpc username and password. */
